358a79444d625bc34f8ed06415a34e0ea5f05324
Amine Zarrouk <amine.zarrouk@esprit.tn>
2024-10-28 22:36:18 +0100

    committ657

--- src/test/java/tn/esprit/tpfoyer17/services/impementations/BlocServiceTestMockito.java
+++ src/test/java/tn/esprit/tpfoyer17/services/impementations/BlocServiceTestMockito.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-class BlocServiceTestMockito {
+class BlocServiceTest {
 
     @InjectMocks
     private BlocService blocService;
@@ -73,67 +73,6 @@ class BlocServiceTestMockito {
         blocService.removeBloc(1L);
 
         // Assert
-        verify(blocRepository, times(1)).deleteById(1L);
-    }
-
-    @Test
-    void testUpdateBloc() {
-        // Arrange
-        Bloc bloc = Bloc.builder().idBloc(1L).nomBloc("UpdatedBloc").build();
-        when(blocRepository.save(any(Bloc.class))).thenReturn(bloc);
-
-        // Act
-        Bloc updatedBloc = blocService.updateBloc(bloc);
-
-        // Assert
-        assertNotNull(updatedBloc);
-        assertEquals("UpdatedBloc", updatedBloc.getNomBloc());
-        verify(blocRepository, times(1)).save(bloc);
-    }
-
-    @Test
-    void testRetrieveBloc() {
-        // Arrange
-        Bloc bloc = Bloc.builder().idBloc(1L).nomBloc("Bloc1").build();
-        when(blocRepository.findById(1L)).thenReturn(Optional.of(bloc));
-
-        // Act
-        Bloc retrievedBloc = blocService.retrieveBloc(1L);
-
-        // Assert
-        assertNotNull(retrievedBloc);
-        assertEquals("Bloc1", retrievedBloc.getNomBloc());
-        verify(blocRepository, times(1)).findById(1L);
-    }
-
-    @Test
-    void testFindByFoyerIdFoyer() {
-        // Arrange
-        Bloc bloc1 = Bloc.builder().nomBloc("Bloc1").build();
-        Bloc bloc2 = Bloc.builder().nomBloc("Bloc2").build();
-        when(blocRepository.findByFoyerIdFoyer(1L)).thenReturn(Arrays.asList(bloc1, bloc2));
-
-        // Act
-        List<Bloc> blocs = blocService.findByFoyerIdFoyer(1L);
-
-        // Assert
-        assertNotNull(blocs);
-        assertEquals(2, blocs.size());
-        verify(blocRepository, times(1)).findByFoyerIdFoyer(1L);
-    }
-
-    @Test
-    void testFindByChambresIdChambre() {
-        // Arrange
-        Bloc bloc = Bloc.builder().idBloc(1L).nomBloc("BlocWithChambre").build();
-        when(blocRepository.findByChambresIdChambre(1L)).thenReturn(bloc);
-
-        // Act
-        Bloc foundBloc = blocService.findByChambresIdChambre(1L);
-
-        // Assert
-        assertNotNull(foundBloc);
-        assertEquals("BlocWithChambre", foundBloc.getNomBloc());
-        verify(blocRepository, times(1)).findByChambresIdChambre(1L);
+        verify(blocRepository, times(1)).delete(blocToDelete);
     }
 }
