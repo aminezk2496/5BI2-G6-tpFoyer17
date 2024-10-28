0d7c1caf8203f0b33f2eca13b52cf1a8e936d287
Amine Zarrouk <amine.zarrouk@esprit.tn>
2024-10-28 20:00:04 +0100

    committ3

--- src/test/java/tn/esprit/tpfoyer17/services/impementations/BlocServiceTest.java
+++ src/test/java/tn/esprit/tpfoyer17/services/impementations/BlocServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import tn.esprit.tpfoyer17.entities.Bloc;
 import tn.esprit.tpfoyer17.repositories.BlocRepository;
 import tn.esprit.tpfoyer17.services.impementations.BlocService;
@@ -14,6 +15,8 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class BlocServiceTest {
 
     @Autowired
