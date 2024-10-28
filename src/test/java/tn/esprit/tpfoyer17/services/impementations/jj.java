56c6db6a87a3d498ac1010869743f8067aa1226f
Syrine Azzouz <syrine.azzouz@esprit.tn>
2024-10-28 23:12:46 +0100

    test unitaire JUNIT ONLY

--- src/test/java/tn/esprit/tpfoyer17/services/impementations/jj.java
+++ src/test/java/tn/esprit/tpfoyer17/services/impementations/jj.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.tpfoyer17.entities.Bloc;
 import tn.esprit.tpfoyer17.entities.Chambre;
 import tn.esprit.tpfoyer17.entities.enumerations.TypeChambre;
@@ -16,7 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
 class ChambreServiceTest {
 
     @Autowired
