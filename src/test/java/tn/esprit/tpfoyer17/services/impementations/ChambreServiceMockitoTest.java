5ba5d457f2c49407bccff53f832d6ed81ee1291b
Syrine Azzouz <syrine.azzouz@esprit.tn>
2024-11-02 01:45:26 +0100

    test2

--- src/test/java/tn/esprit/tpfoyer17/services/impementations/ChambreServiceMockitoTest.java
+++ src/test/java/tn/esprit/tpfoyer17/services/impementations/ChambreServiceMockitoTest.java
@@ -37,38 +37,9 @@ class ChambreServiceMockitoTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    void testRetrieveAllChambres() {
-        // Arrange
-        Chambre chambre1 = Chambre.builder().numeroChambre(101L).typeChambre(TypeChambre.SIMPLE).build();
-        Chambre chambre2 = Chambre.builder().numeroChambre(102L).typeChambre(TypeChambre.DOUBLE).build();
-        when(chambreRepository.findAll()).thenReturn(Arrays.asList(chambre1, chambre2));
 
-        // Act
-        List<Chambre> chambres = chambreService.retrieveAllChambres();
 
-        // Assert
-        assertNotNull(chambres);
-        assertEquals(2, chambres.size());
-        assertEquals(101L, chambres.get(0).getNumeroChambre());
-        assertEquals(102L, chambres.get(1).getNumeroChambre());
-        verify(chambreRepository, times(1)).findAll();
-    }
 
-    @Test
-    void testAddChambre() {
-        // Arrange
-        Chambre chambre = Chambre.builder().numeroChambre(103L).typeChambre(TypeChambre.SIMPLE).build();
-        when(chambreRepository.save(chambre)).thenReturn(chambre);
-
-        // Act
-        Chambre savedChambre = chambreService.addChambre(chambre);
-
-        // Assert
-        assertNotNull(savedChambre);
-        assertEquals(103L, savedChambre.getNumeroChambre());
-        verify(chambreRepository, times(1)).save(chambre);
-    }
 
     @Test
     void testAffecterChambresABloc() {
